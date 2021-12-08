ed46034255e6e00c50152d96e764bacbb566ceab
AidaFig <aidafigueiredo8@gmail.com>
2021-12-08 19:44:20 -0300

    creating the Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./FooterStyle.css";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&#169; 2021 | Desenvolvido por <strong>Aida Figueiredo</strong></p>
+        </footer>
+    )
+}
