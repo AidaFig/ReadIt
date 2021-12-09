cb7c8cdd535f3c8af1c88ec83aea040232b19b58
AidaFig <aidafigueiredo8@gmail.com>
2021-12-09 15:33:59 -0300

    updatinh some Nav styles and making it responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,17 +5,16 @@ export const Nav = () => {
     return (
         <div className="container">
             <nav className="nav">
-                <img className="logo" src="../../assets/icons/shopping-cart (1).png" alt="" />
                 <h1 className="title">Read It!</h1>
                 <ul className="menu-container">
                     <li>
                         <Link to="/">Sobre</Link>
                     </li>
                     <li>
-                        <Link to="">Books</Link>
+                        <Link to="">Livros</Link>
                     </li>
                     <li>
-                        <Link to="">My Library</Link>
+                        <Link to="">Minha Biblioteca</Link>
                     </li>
                 </ul>
             </nav>
