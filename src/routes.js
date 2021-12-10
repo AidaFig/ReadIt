a1404fbcd05bdf0b84765214cdf755c592846a82
AidaFig <aidafigueiredo8@gmail.com>
2021-12-10 15:19:13 -0300

    addind Books route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Books } from "./pages/Books/Books";
 
 import { Sobre } from "./pages/Sobre/Sobre"
 
@@ -8,6 +9,7 @@ function Routes () {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Sobre}/>
+                <Route path="/livros" exact component={Books}/>
             </Switch>
         </BrowserRouter>
     )
