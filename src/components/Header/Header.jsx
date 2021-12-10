212075bf397c9150646d46cbdacfad87d72ea3a0
AidaFig <aidafigueiredo8@gmail.com>
2021-12-10 15:19:44 -0300

    adding propos to the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import { Button } from "../Button/Button";
 import "./HeaderStyle.css";
 
-export const Header = () => {
+export const Header = ({img, children, alt}) => {
     return (
         <header className="header">
             <div className="header-container">
                 <div className="header-box_img">
-                    <img className="img-header" src="../../assets/images/img-header.svg" alt=""/>
+                    <img className="img-header" src={img} alt={alt}/>
                 </div>
                 <div className="header-box_text">
-                    <h1 className="title-header"> Read It!</h1>
-                    <p>Oferecendo oportunidades de leitura online a quem não vive sem um livrinho. Não importa onde, como ou quando, você pode abrir o seu livro e ler. </p>
-
+                    <h1 className="title-header">read it!</h1>
+                    <p>{children}</p>
                     <Button>Saiba Mais</Button>
                 </div>
             </div>
