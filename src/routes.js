42e8f6840ccd92b7b10ded53044891e7a0b7b9ee
AidaFig <aidafigueiredo8@gmail.com>
2021-12-16 15:20:54 -0300

    creating the romance route

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Books } from "./pages/Books/Books";
-
-import { Sobre } from "./pages/Sobre/Sobre"
+import { Sobre } from "./pages/Sobre/Sobre";
+import { Romance } from "./pages/Romance/Romance";
 
 function Routes () {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Sobre}/>
-                <Route path="/livros" exact component={Books}/>
+                <Route path="/" exact component={Sobre} />
+                <Route path="/livros" exact component={Books} />
+                <Route path="/romance" exact component={Romance} />
             </Switch>
         </BrowserRouter>
     )
