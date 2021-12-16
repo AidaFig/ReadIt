67dfee7132cea268e5fa8361dc30e3d044dfa296
AidaFig <aidafigueiredo8@gmail.com>
2021-12-16 19:06:44 -0300

    adding Biography route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Books } from "./pages/Books/Books";
 import { Sobre } from "./pages/Sobre/Sobre";
 import { Romance } from "./pages/Romance/Romance";
+import { Biography } from "./pages/Biography/Biography";
 
 function Routes () {
     return (
@@ -11,6 +12,7 @@ function Routes () {
                 <Route path="/" exact component={Sobre} />
                 <Route path="/livros" exact component={Books} />
                 <Route path="/romance" exact component={Romance} />
+                <Route path="/biografia" exact component={Biography} />
             </Switch>
         </BrowserRouter>
     )
