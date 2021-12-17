fc0035d56e0d4b11af3344ae7ad516f149afbd97
AidaFig <aidafigueiredo8@gmail.com>
2021-12-16 21:12:18 -0300

    adding components to the page

--- src/pages/Horror/Horror.jsx
+++ src/pages/Horror/Horror.jsx
@@ -1,7 +1,13 @@
-import { Nav } from "../../components/Nav/Nav"
+import { Nav } from "../../components/Nav/Nav";
+import { Arrow } from "../../components/Arrow/Arrow";
+import { Input } from "../../components/Input/Input";
 
 export const Horror = () => {
     return(
-        <Nav />
+        <>
+            <Nav />
+            <Input />
+            <Arrow />
+        </>
     )
 }
