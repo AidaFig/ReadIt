0d98ad5b57ce91b95fe2f985c88abe68f5a3d780
AidaFig <aidafigueiredo8@gmail.com>
2021-12-17 14:12:33 -0300

    creating the component Actions

---
+++ src/components/Actions/Actions.jsx
@@ -0,0 +1,11 @@
+import "./ActionsStyles.css";
+
+export const Actions = () => {
+    return (
+        <div className="icons-actions_container">
+            <img className="icons-actions"src="../assets/icons/add.png" alt="add" />
+            <img className="icons-actions"src="../assets/icons/more.png" alt="lendo" />
+            <img className="icons-actions"src="../assets/icons/check.png" alt="" />
+        </div>
+    )
+}
