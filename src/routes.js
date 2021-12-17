93de12728f08c93bfacd002e7c81dfb214580695
AidaFig <aidafigueiredo8@gmail.com>
2021-12-16 21:04:58 -0300

    creatin the Horror route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Books } from "./pages/Books/Books";
 import { Sobre } from "./pages/Sobre/Sobre";
 import { Romance } from "./pages/Romance/Romance";
 import { Biography } from "./pages/Biography/Biography";
+import { Horror } from "./pages/Horror/Horror";
 
 function Routes () {
     return (
@@ -13,6 +14,7 @@ function Routes () {
                 <Route path="/livros" exact component={Books} />
                 <Route path="/romance" exact component={Romance} />
                 <Route path="/biografia" exact component={Biography} />
+                <Route path="/terror" exact component={Horror} />
             </Switch>
         </BrowserRouter>
     )
